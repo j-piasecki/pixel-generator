2e89113bd2fca3d54a5920f7aa01c1c0dc1ebf17
j-piasecki <jakubpiasecki67@gmail.com>
2019-12-07 19:52:07 +0100

    Add steps to circular gradient

--- js/drawing/brushes/brushCircularGradient.mjs
+++ js/drawing/brushes/brushCircularGradient.mjs
@@ -19,24 +19,67 @@ export class BrushCircularGradient extends Brush {
             outerColor = new Color(0, 0, 0, 0);
         }
 
-        this.innerColor = innerColor;
-        this.outerColor = outerColor;
+        this.steps = [new GradientStep(0, innerColor), new GradientStep(1, outerColor)];
         
-        this.invertedColor = new Color(255 - (innerColor.r + outerColor.r) / 2, 255 - (innerColor.g + outerColor.g) / 2, 255 - (innerColor.b + outerColor.b) / 2, 1);
-
+        this.width = width;
+        this.height = height;
         this.radius = radius;
         this.center = center;
 
-        for (let x = 0; x < width; x++) {
-            for (let y = 0; y < height; y++) {
-                let progress = new Vector2(x, y).distanceFrom(this.center) / this.radius;
+        this.generate();
+    }
 
+    /**
+     * Generates layer used for drawing with this brush
+     */
+    generate() {
+        for (let x = 0; x < this.width; x++) {
+            for (let y = 0; y < this.height; y++) {
+                let progress = new Vector2(x, y).distanceFrom(this.center) / this.radius;
+                
                 if (progress > 1) {
                     progress = 1;
                 }
-                
-                this.layer.setPixel(x, y, this.innerColor.transition(this.outerColor, progress));
+
+                for (let i = 0; i < this.steps.length - 1; i++) {
+                    if (this.steps[i].progress <= progress && this.steps[i + 1].progress >= progress) {
+                        let scaledProgress = (progress - this.steps[i].progress) / (this.steps[i + 1].progress - this.steps[i].progress);
+
+                        this.layer.setPixel(x, y, this.steps[i].color.transition(this.steps[i + 1].color, scaledProgress));
+                    }
+                }
             }
         }
+        
+        this.invertedColor = new Color(0, 0, 0, 1);
+
+        for (let i = 0; i < this.steps.length; i++) {
+            this.invertedColor.r += this.steps[i].color.r;
+            this.invertedColor.g += this.steps[i].color.g;
+            this.invertedColor.b += this.steps[i].color.b;
+        }
+
+        this.invertedColor.r = Math.floor(255 - (this.invertedColor.r / this.steps.length));
+        this.invertedColor.g = Math.floor(255 - (this.invertedColor.g / this.steps.length));
+        this.invertedColor.b = Math.floor(255 - (this.invertedColor.b / this.steps.length));
+    }
+
+    /**
+     * Adds step to the gradient at specified range
+     * @param {Number} progress - Normalized distance from the center (0-1)
+     * @param {Color} color - Color of the step
+     */
+    addStep(progress, color) {
+        this.steps.push(new GradientStep(progress, color));
+        this.steps.sort((a, b) => { return a.progress - b.progress; });
+
+        this.generate();
+    }
+}
+
+class GradientStep {
+    constructor(progress, color) {
+        this.progress = progress;
+        this.color = color;
     }
 }
